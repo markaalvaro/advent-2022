c6ef4cbf570e734f1b33590eaec1827ff3061dbf
Mark Alvaro <markaalvaro@gmail.com>
2022-12-05 16:02:28 -0500

    Minor cleanup for readability to day 5.

--- src/main/kotlin/com/github/markaalvaro/advent2022/Day05.kt
+++ src/main/kotlin/com/github/markaalvaro/advent2022/Day05.kt
@@ -7,10 +7,11 @@ fun supplyStacks1() = supplyStacks(false)
 fun supplyStacks2() = supplyStacks(true)
 
 fun supplyStacks(reversed: Boolean): String {
-    val startingPosition = readFile(FILE_NAME, trim = false).takeWhile { it.isNotBlank() }
+    val startingPosition = readFile(FILE_NAME, trim = false)
+        .takeWhile { it.isNotBlank() }
+        .dropLast(1)
         .filter { it.length >= 2 }
         .map { it.chunked(4).map { chunk -> chunk[1] } }
-        .dropLast(1)
 
     val stacks = mutableListOf<ArrayDeque<Char>>()
 
@@ -18,17 +19,18 @@ fun supplyStacks(reversed: Boolean): String {
         stacks.add(ArrayDeque())
         var i = startingPosition.lastIndex
         do {
-            stacks.last().add(startingPosition[i][j])
-        } while (--i >= 0 && j <= startingPosition[i].lastIndex && startingPosition[i][j] != ' ')
+            val current = startingPosition[i][j]
+            if (current != ' ') stacks.last().add(startingPosition[i][j])
+        } while (--i >= 0 && j <= startingPosition[i].lastIndex)
     }
 
     readFile(FILE_NAME).dropWhile { it.isNotBlank() }
         .drop(1)
-        .map { it.split(" ").filter { d -> !d.matches(Regex("[a-z]*")) } }
+        .map { it.split(" ").mapNotNull { d -> d.toIntOrNull() } }
         .forEach { (toMove, from, to) ->
-            val buffer = (1..toMove.toInt()).map { stacks[from.toInt() - 1].removeLast() }.toMutableList()
+            val buffer = (1..toMove).map { stacks[from - 1].removeLast() }.toMutableList()
             if (reversed) buffer.reverse()
-            buffer.forEach { stacks[to.toInt() - 1].add(it) }
+            buffer.forEach { stacks[to - 1].add(it) }
         }
 
     return stacks.map { it.removeLast() }.joinToString("")
